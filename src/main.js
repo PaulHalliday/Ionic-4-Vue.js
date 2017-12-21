1872588dd822ffd2bc38c6c534bbcb72e1be71b6
PaulHalliday <hello@paulhalliday.io>
2017-12-21 03:28:27 +0000

    refactor(main.js): Refactored Vue.config.ignoredElements to take a RegExp instead of string componen
    Fix #1

--- src/main.js
+++ src/main.js
@@ -2,18 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 
-Vue.config.ignoredElements = [
-  'ion-app',
-  'ion-page',
-  'ion-header',
-  'ion-toolbar',
-  'ion-title',
-  'ion-content',
-  'ion-list',
-  'ion-item',
-  'ion-label',
-  'ion-button'
-]
+Vue.config.ignoredElements = [/^ion-/]
 
 new Vue({
   el: '#app',
